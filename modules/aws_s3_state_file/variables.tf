599b3b9269844979ac184dceadea1f08929ef2ab
rachakondadharmendra <rachakondadharmendrainfo@gmail.com>
2023-10-04 14:28:43 +0530

    Adding the updated terraform files

--- modules/aws_s3_state_file/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  description = "Remote S3 Bucket Name"
-  type        = string
-}
-
-variable "table_name" {
-  description = "Remote DynamoDB Table Name"
-  type        = string
-}
